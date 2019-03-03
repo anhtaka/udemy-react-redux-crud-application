6a095c9dfe9fd35657d2f21c222dd8baccbc0735
anhtaka <email@example.com>
2019-03-03 17:34:23 +0900

    learn state and setSate

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-const App = () => {
-  const profiles = [
-    {name: "Taro",age:10},
-    {name: "Hanako",age:5},
-    {name: "Noname"}
-  ]
-  return (
-    <div>
-    {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-    }
-    </div>
-  )
-}
+const App = () => (<Counter></Counter>)
 
+class Counter extends Component{
+  constructor(props){
+    super(props)
+    console.log(this.state)
+    this.state = {count: 0}
+  }
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1})
+  }
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1})
+  }
+  render() {
+    console.log("render")
+    return (
+      <React.Fragment>
+      <div>counter: {this.state.count }</div>
+      <button onClick={this.handlePlusButton}>+1</button>
+      <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
 
-const User = (props) => {
-  return <div>Hi, I am {props.name} and {props.age} years old</div>
-}
-User.defaultProps = { 
-  age: 1
+  }
 }
 
 export default App;
